5c87a019e585c28c242fea9aa28b9139c8ba6a87
IllegalCreed <illegals0001@gmail.com>
2026-01-15 10:20:44 +0800

    chore(backend): require explicit env file (ENV_FILE or NODE_ENV) in db-utils; seed wrapper sets ENV_FILE; remove low-level seed files

--- apps/quiz-backend/prisma/seed-system.ts
+++
@@ -1,8 +0,0 @@
-import { seedSystem } from "./db-utils";
-
-seedSystem()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(() => process.exit(0));
--- apps/quiz-backend/prisma/seed-test.ts
+++
@@ -1,8 +0,0 @@
-import { seedTest } from "./db-utils";
-
-seedTest()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(() => process.exit(0));
