ebe09af2091edf4468016cc232d36b142eb04659
IllegalCreed <illegals0001@gmail.com>
2026-01-19 15:57:48 +0800

    chore(storybook): remove docs/autodocs and uninstall @storybook/addon-docs; fix eslint tsconfig

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -4,13 +4,10 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/stories/**/*.stories.@(ts|tsx|mdx)"],
-  addons: ["@storybook/addon-essentials"],
+  addons: [],
   framework: {
     name: "@storybook/vue3-vite",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
   },
   async viteFinal(config_, { configType }) {
     return mergeConfig(config_, {
