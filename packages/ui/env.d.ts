d0a512bd9e80b661ba9bab0ea0ac9130732a82a5
IllegalCreed <illegals0001@gmail.com>
2026-01-19 16:02:11 +0800

    chore(types): add packages/ui/env.d.ts for non-TS module declarations

---
+++ packages/ui/env.d.ts
@@ -0,0 +1,7 @@
+// Type declarations for non-TS module imports used by the package
+declare module "uno.css";
+declare module "*.css";
+declare module "*.scss";
+declare module "*.svg";
+declare module "*.png";
+declare module "*.jpg";
