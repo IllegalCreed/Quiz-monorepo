774025433bb823319c22c1d692c53b8813c41338
IllegalCreed <illegals0001@gmail.com>
2026-02-02 17:55:37 +0800

    build(ui): add dts generation (tsconfig, shims) and update build:types

---
+++ packages/ui/src/components/CheckRadioGroup.vue.d.ts
@@ -0,0 +1,7 @@
+import type { DefineComponent } from "vue";
+declare const component: DefineComponent<
+  Record<string, unknown>,
+  Record<string, unknown>,
+  unknown
+>;
+export default component;
---
+++ packages/ui/src/components/shims.d.ts
@@ -0,0 +1,19 @@
+declare module "./CheckRadio.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    unknown
+  >;
+  export default component;
+}
+
+declare module "./CheckRadioGroup.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    unknown
+  >;
+  export default component;
+}
