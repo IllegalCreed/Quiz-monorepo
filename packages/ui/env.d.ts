74906a4483838f74b47658260ee68435157353e6
IllegalCreed <illegals0001@gmail.com>
2026-02-02 17:20:44 +0800

    fix: 修复ui库声明问题

--- packages/ui/env.d.ts
+++ packages/ui/env.d.ts
@@ -7,3 +7,9 @@ declare module "*.scss";
 declare module "*.svg";
 declare module "*.png";
 declare module "*.jpg";
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
